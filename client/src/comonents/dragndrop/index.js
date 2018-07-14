d04e62f031895be5bbf10b409fad3ce5eafa69ee
Tim Tucker <tntucker0015@outlook.com>
2018-07-14 17:04:55 -0400

    git granch

---
+++ client/src/comonents/dragndrop/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppDragDropDemo from './dragndrop/AppDragDropDemo';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<AppDragDropDemo />, document.getElementById('root'));
+registerServiceWorker();
