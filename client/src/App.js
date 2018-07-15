65286b9ca208fd1d4bbf9ba8cb3197c95bda3fb9
Anthony J Lappy <anthonyj.manenti@gmail.com>
2018-07-15 14:58:36 -0400

    fixed small case issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './index.css';
-import FlipPage from './components/FlipPage/FlipPage.js';
+import FlipPage from './components/flipPage/FlipPage.js';
 
 
 class App extends Component {
