c2eee3d8e0e1fe5927c6d2fc65da6a8dac891b6d
Tim Tucker <tntucker0015@outlook.com>
2018-07-28 11:24:04 -0400

    moving dragdrop to main app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,83 @@
 import React, { Component } from 'react';
+import './App.css';
 import './index';
 import FlipPage from './components/flipPage/FlipPage.js';
-import DragDrop from './components/dragndrop/DragDrop';
+import banana from './components/image/cartoonyBananas.png';
+import burger from './components/image/hamburger.png';
+import lolli from './components/image/lollipop.png';
 
 class App extends Component {
-  render() {
+  state = {
+    tasks: [
+        {name:"banana",category:"readyItem", bgimage: banana},
+        {name:"burger", category:"readyItem", bgimage: burger},
+        {name:"lolli", category:"readyItem", bgimage: lolli}
+      ]
+}
+
+onDragStart = (ev, id) => {
+    console.log('dragstart:',id);
+    ev.dataTransfer.setData("id", id);
+}
+
+onDragOver = (ev) => {
+    ev.preventDefault();
+}
+
+onDrop = (ev, cat) => {
+   let id = ev.dataTransfer.getData("id");
+   
+   let tasks = this.state.tasks.filter((task) => {
+       if (task.name === id) {
+           task.category = cat;
+       }
+       return task;
+   });
+
+   //needs to go in app.js and passed as props this.props.nameOfFunction() -- in app.js they will be state
+   this.setState({
+       ...this.state,
+       tasks
+   });
+}
+
+render() {
+    var tasks = {
+        readyItem: [],
+        dropped: []
+    }
+
+    this.state.tasks.forEach ((t) => {
+        tasks[t.category].push(
+            <div key={t.name} 
+                onDragStart = {(e) => this.onDragStart(e, t.name)}
+                draggable
+                className="draggable"
+                id = {t.name}
+            >
+            </div>
+        );
+    });
+
     return (
-      <div className="container-fluid">
-        <FlipPage />
-        <DragDrop />
-      </div>
+        <div className="container-drag">
+            <h2 className="header">Baby Bear Books</h2>
+            <div className="readyItem"
+                onDragOver={(e)=>this.onDragOver(e)}
+                onDrop={(e)=>{this.onDrop(e, "readyItem")}}>
+                <span className="task-header">Drag Me!</span>
+                {tasks.readyItem}
+            </div>
+<FlipPage />
+            <div className="droppable" 
+                onDragOver={(e)=>this.onDragOver(e)}
+                onDrop={(e)=>this.onDrop(e, "dropped")}>
+                <span className="task-header"></span>
+                {tasks.dropped}
+            </div>
+        </div>
     );
-  }
+}
 }
 
 export default App;
--- client/src/components/dragndrop/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import DragDrop from '../../../DragDrop';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<DragDrop />, document.getElementById('root'));
-registerServiceWorker();
